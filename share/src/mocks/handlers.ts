97b40638365b1eb30bb4a913c256952c5a27f123
chaospace <ichaospace@gmail.com>
2024-05-08 16:39:09 +0900

    [share] pokemon생성 mock 추가

--- share/src/mocks/handlers.ts
+++ share/src/mocks/handlers.ts
@@ -4,6 +4,12 @@ import { pokemons } from "./response/pokemons";
 const handlers = [
     http.get("http://api.example.com/pokemons", async () => {
         return HttpResponse.json(pokemons);
+    }),
+
+    http.post("http://api.example.com/pokemons", async ({ request }) => {
+        const newPokemons = await request.json();
+        //201상태를 응답해 자원생성을 알림.
+        return HttpResponse.json(newPokemons, { status: 201 });
     })
 ];
 
