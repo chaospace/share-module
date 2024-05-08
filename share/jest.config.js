ba357d44205d96d8e8c5cdb6f2f22b6889887650
chaospace <ichaospace@gmail.com>
2024-05-08 15:54:01 +0900

    [share/설정] jest관련 모듈 추가

---
+++ share/jest.config.js
@@ -0,0 +1,24 @@
+const config = {
+  verbose: true,
+  rootDir: ".",
+  roots: ["<rootDir>/src/", "<rootDir>/tests/"],
+  // testEnvironment: "node",
+  transformIgnorePatterns: ["/node_modules/", "/dist/", "\\.pnp\\.[^\\/]+$"],
+  coveragePathIgnorePatterns: ["src/index.ts"],
+  setupFilesAfterEnv: ["./tests/setup.ts"],
+  testMatch: [
+    "<rootDir>/src/**/*.(test|spec).(ts|tsx)",
+    "<rootDir>/tests/**/*.(test|spec).(ts|tsx)",
+  ],
+  transform: {
+    "^.+\\.tsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.+)$": "<rootDir>/src/$1",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/tests/__mocks__/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/tests/__mocks__/styleMock.ts",
+  },
+};
+
+module.exports = config;
