3ea475f21c27f94062387f93457c398af86d6830
chaospace <ichaospace@gmail.com>
2024-05-09 14:30:15 +0900

    [share] jest 테스트환경 jsdom에서 msw/node 모듈 참조 에러 수정

--- share/jest.config.ts
+++ share/jest.config.ts
@@ -3,10 +3,15 @@ const config = {
   verbose: true,
   rootDir: ".",
   roots: ["<rootDir>/src/", "<rootDir>/tests/"],
-  // testEnvironment: "node",
+  // testEnvironment: "jsdom",
+  // msw에 node사용을 위해 커스텀 옵션을 빈 문자열로 변경 jsdom사용 시 기본은 browser로 된다.
+  // testEnvironmentOptions: {
+  //   customExportConditions: [''],
+  // },
   transformIgnorePatterns: ["/node_modules/", "/dist/", "\\.pnp\\.[^\\/]+$"],
   coveragePathIgnorePatterns: ["src/index.ts"],
-  setupFilesAfterEnv: ["./tests/setup.ts"],
+  setupFiles: ["./tests/jest.pollyfill.ts",],
+  setupFilesAfterEnv: ["./tests/jest.setup.ts"],
   testMatch: [
     "<rootDir>/src/**/*.(test|spec).(ts|tsx)",
     "<rootDir>/tests/**/*.(test|spec).(ts|tsx)",
@@ -14,6 +19,7 @@ const config = {
   transform: {
     "^.+\\.tsx?$": "babel-jest",
   },
+  resolver: "<rootDir>/tests/jest.resolver.js",
   moduleNameMapper: {
     "^@/(.+)$": "<rootDir>/src/$1",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
