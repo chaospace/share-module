a94d77b0e28f5142600c2bc3fe926b7b818279f0
chaospace <ichaospace@gmail.com>
2024-05-14 12:00:07 +0900

    [front] StyledButton 추가

--- front/src/components/elements/sample/StyledButton.tsx
+++ front/src/components/elements/sample/StyledButton.tsx
@@ -1,10 +1,15 @@
-import { ColorCategory, VariantCategory } from "@/@types/styled";
+import { getVariantColor } from "@/styles/utils";
+import { VariantCategory } from "styled";
 import styled from "styled-components";
+import { variant } from "@/colors";
 
 interface StyledButtonProps {
-    color?: ColorCategory;
     variant?: VariantCategory;
 }
+
+const backgroundColor = getVariantColor("variant", "main", variant);
+const textColor = getVariantColor("variant", "light", variant);
+const borderColor = getVariantColor("variant", "dark", variant);
 
 const StyledButton = styled.button<StyledButtonProps>`
     font:inherit;
@@ -15,9 +20,9 @@ const StyledButton = styled.button<StyledButtonProps>`
     border-width: 1px;
     border-style: solid;
     cursor:pointer;
-    background-color:${(props) => props.theme.variant[props.variant!].main};
-    color:${(props) => props.theme.variant[props.variant!].light};
-    border-color:${(props) => props.theme.variant[props.variant!].dark};
+    background-color:${backgroundColor};
+    color:${textColor};
+    border-color:${borderColor};
 `;
 
 StyledButton.defaultProps = {
