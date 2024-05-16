f213d0392dd534e84775b48ed9bf75bc45c02234
chaospace <ichaospace@gmail.com>
2024-05-16 13:22:07 +0900

    [front] element 폴더 이동

--- front/src/components/elements/button/StyledButton.tsx
+++ front/src/components/elements/button/StyledButton.tsx
@@ -1,30 +1,35 @@
-import { getVariantColor } from "@/styles/utils";
+import { curriedShouldForwardProp, getVariantColor } from "@/styles/utils";
 import { VariantCategory } from "styled";
 import styled from "styled-components";
 import { variant } from "@/colors";
 
 interface StyledButtonProps {
     variant?: VariantCategory;
+    borderRadius?: string;
+    padding?: string;
+    color?: string;
 }
 
 const backgroundColor = getVariantColor("variant", "main", variant);
 const textColor = getVariantColor("variant", "light", variant);
 const borderColor = getVariantColor("variant", "dark", variant);
 
-const StyledButton = styled.button<StyledButtonProps>`
+const StyledButton = styled.button.withConfig({
+    shouldForwardProp: curriedShouldForwardProp(['color', "borderRadius", "padding"])
+}) <StyledButtonProps>`
     font:inherit;
-    appearance: none;
-    border:none;
     padding:0.5rem 1rem;
     border-radius:.25rem;
-    border-width: 1px;
-    border-style: solid;
-    cursor:pointer;
     background-color:${backgroundColor};
     color:${textColor};
     border-color:${borderColor};
+    
+    &:hover {
+        background-color:${backgroundColor};
+        color:${textColor}
+    }
 `;
-
+// ExecutionContext & FastOmit<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, keyof StyledButtonProps> & StyledButtonProps>
 StyledButton.defaultProps = {
     variant: "default"
 }
--- front/src/components/elements/layout/Box.ts
+++ front/src/components/elements/layout/Box.ts
@@ -1,12 +1,17 @@
+//import { HTMLAttributes } from "react";
+import { curriedShouldForwardProp } from "@/styles/utils";
 import styled from "styled-components";
 
+// default style
 interface BoxProps {
     direction?: "column" | "row";
     gap?: number;
     padding?: string;
 }
 
-const Box = styled.div<BoxProps>`
+const Box = styled.div.withConfig({
+    shouldForwardProp: curriedShouldForwardProp(['gap', 'direction', 'padding'])
+}) <BoxProps>`
     position: relative;
     display: flex;
     flex-direction: ${props => props.direction};
