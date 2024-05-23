609183f911c36633b78800f222f6f08eb2154e54
chaospace <ichaospace@gmail.com>
2024-05-23 16:19:32 +0900

    [설정] prettier 모듈 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,69 +1,77 @@
 // import { fixupPluginRules } from "@eslint/compat";
-const tseslint = require("typescript-eslint");
-const eslintReactRecommended = require("eslint-plugin-react");
-const eslintReactHooks = require("eslint-plugin-react-hooks");
-const { fixupPluginRules } = require("@eslint/compat");
+const tseslint = require('typescript-eslint');
+const eslintReactRecommended = require('eslint-plugin-react');
+const eslintReactHooks = require('eslint-plugin-react-hooks');
+//const eslitPrettier = require('prettier');
+const { fixupPluginRules } = require('@eslint/compat');
+const eslintPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 const tsconfig = tseslint.config({
-  files: ["src/**/*.{tsx,ts}"],
+  files: ['src/**/*.{tsx,ts}'],
   plugins: {
-    "@typescript-eslint": tseslint.plugin,
+    '@typescript-eslint': tseslint.plugin
   },
   languageOptions: {
     parser: tseslint.parser,
     parserOptions: {
       project: true, // 실행 위치에서 가장 가까운 tsconfig참조
-      tsConfigRootDir: __dirname,
-    },
+      tsConfigRootDir: __dirname
+    }
   },
   rules: {
-    "jsx-quotes": ["warn", "prefer-single"],
-    "@typescript-eslint/quotes": ["warn", "single", { avoidEscape: true }],
-    "@typescript-eslint/no-unused-vars": [
-      "error",
+    'jsx-quotes': ['warn', 'prefer-single'],
+    '@typescript-eslint/quotes': ['warn', 'single', { avoidEscape: true }],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
-        destructuredArrayIgnorePattern: "^_",
-        caughtErrorsIgnorePattern: "^_",
-      },
-    ],
-  },
-});
-
-const arules = {};
-for (let p in eslintReactRecommended.rules) {
-  if (p !== "react/no-string-refs") {
-    arules[p] = eslintReactRecommended.rules[p];
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        destructuredArrayIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_'
+      }
+    ]
   }
-}
+});
 
 const reactRecommended = {
   // files: ["src/**/*.{tsx,ts}"],
   plugins: {
-    react: fixupPluginRules(eslintReactRecommended),
+    react: fixupPluginRules(eslintReactRecommended)
   },
   languageOptions: {
-    ...eslintReactRecommended.configs.recommended.languageOptions,
+    ...eslintReactRecommended.configs.recommended.languageOptions
   },
   rules: {
     ...eslintReactRecommended.configs.recommended.rules,
+    'react/no-unescaped-entities': 'off'
   },
   settings: {
     react: {
-      version: "18.3.1",
-    },
-  },
+      version: '18.3.1'
+    }
+  }
 };
 const reactHooks = {
-  // files: ["src/**/*.{tsx,ts}"],
   plugins: {
-    "react-hooks": fixupPluginRules(eslintReactHooks),
+    'react-hooks': fixupPluginRules(eslintReactHooks)
   },
   rules: {
-    ...eslintReactHooks.configs.recommended.rules,
-    "react/no-unescaped-entities": "off",
-  },
+    ...eslintReactHooks.configs.recommended.rules
+  }
+};
+
+const prettierRecommended = {
+  ...eslintPrettierRecommended,
+  rules: {
+    ...eslintPrettierRecommended.rules,
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true
+      }
+    ]
+  }
 };
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
@@ -71,15 +79,17 @@ const config = [
   ...tsconfig,
   reactRecommended,
   reactHooks,
+  eslintPrettierRecommended,
   {
     ignores: [
-      "node_modules/**",
-      "dist/**",
-      "config/**",
-      "assets/**",
-      ".storybook/**",
-    ],
-  },
+      'node_modules/**',
+      'dist/**',
+      'config/**',
+      'assets/**',
+      '.storybook/**',
+      'storybook-static/**'
+    ]
+  }
 ];
 
 module.exports = config;
