b1ce8737e101541ed7189216ce52432318717a34
chaospace <ichaospace@gmail.com>
2024-05-23 21:43:53 +0900

    [share] 불필요 파일 제거 및 코드 포맷 적용

--- share/src/store/types.ts
+++ share/src/store/types.ts
@@ -1,43 +1,50 @@
 import { StateCreator, StoreApi } from 'zustand';
 
-
-type StateUpdater<S> = ((prev: S) => S);
+type StateUpdater<S> = (prev: S) => S;
 
 type PropsSelector<State> = {
-    [Property in keyof State as `${Property & string}Selector`]: (state: State) => State[Property];
-}
+  [Property in keyof State as `${Property & string}Selector`]: (state: State) => State[Property];
+};
 
 type Setter<State> = {
-    [Property in keyof State as `set${Capitalize<Lowercase<Property & string>>}`]: (nextState: State[Property] | StateUpdater<State[Property]>) => void;
-}
+  [Property in keyof State as `set${Capitalize<Lowercase<Property & string>>}`]: (
+    nextState: State[Property] | StateUpdater<State[Property]>
+  ) => void;
+};
 
 // type SetterSelector<T, K extends keyof T = keyof T> = {
 //     [P in K as P extends `set${infer U}` ? `set${U}Selector` : never]: (state: T) => T[P]
 // }
 
 type SetterKeys<State, Key extends keyof State = keyof State> = Key extends `set${infer U}`
-    ? `set${U}`
-    : never;
+  ? `set${U}`
+  : never;
 
-
-type State<T> = T & Setter<T>
+type State<T> = T & Setter<T>;
 //type StateSetSelector<S> = SetterSelector<S>;
 
 type StateHooks<S, K extends keyof S = keyof S> = {
-    [P in K as `use${Capitalize<P & string>}`]: () => S[P]
-}
+  [P in K as `use${Capitalize<P & string>}`]: () => S[P];
+};
 
-type StateHookCreator<State, Store extends StoreApi<State> = StoreApi<State>> = (store: Store) => StateHooks<State>;
+type StateHookCreator<State, Store extends StoreApi<State> = StoreApi<State>> = (
+  store: Store
+) => StateHooks<State>;
 
-type StateCreatorEnhancer<State> = StateCreator<State, [['zustand/immer', never], ['zustand/devtools', never]], [], State>;
+type StateCreatorEnhancer<State> = StateCreator<
+  State,
+  [['zustand/immer', never], ['zustand/devtools', never]],
+  [],
+  State
+>;
 
 export type {
-    State,
-    SetterKeys,
-    PropsSelector,
-    Setter,
-    // StateSetSelector,
-    StateHooks,
-    StateHookCreator,
-    StateCreatorEnhancer
-}
+  State,
+  SetterKeys,
+  PropsSelector,
+  Setter,
+  // StateSetSelector,
+  StateHooks,
+  StateHookCreator,
+  StateCreatorEnhancer
+};
