f4f0a420b9076272c67ba69f224741501cd53321
chaospace <ichaospace@gmail.com>
2024-05-25 01:33:05 +0900

    [front] typography컴포넌트 개선

---
+++ front/src/components/elements/Typography.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+import { CSSComposerObject, composer } from 'styled-composer';
+import { shouldForwardCSSProps } from '@/styles/utils';
+import { KnownTarget } from 'styled-components/dist/types';
+
+const typoVariant = {
+  title: {
+    fontSize: '2rem',
+    lineHeight: 2
+  },
+  subTitle1: {
+    fontSize: '1.6rem',
+    lineHeight: 1.6
+  },
+  subTitle2: {
+    fontSize: '1.4rem',
+    lineHeight: 1.4
+  },
+  body: {
+    fontSize: '1rem'
+  },
+  caption: {
+    fontSize: '.8rem',
+    lineHeight: 0.8
+  }
+};
+
+type TypoVariant = keyof typeof typoVariant;
+
+interface TypographyProps extends CSSComposerObject {
+  variant?: TypoVariant;
+}
+
+const getPolymorphicTag = (variant: TypoVariant) => {
+  switch (variant) {
+    case 'title':
+      return 'h1';
+    case 'subTitle1':
+      return 'h2';
+    case 'subTitle2':
+      return 'h3';
+    case 'body':
+      return 'p';
+    case 'caption':
+      return 'span';
+  }
+};
+// 이 방식에 문제점. as속성이 없으면 기본속성을 추론 못함.
+const Typography = styled('p')
+  .attrs<TypographyProps & { as?: KnownTarget }>(({ variant, as, ...rest }) => ({
+    ...typoVariant[variant!],
+    ...rest,
+    as: as ?? getPolymorphicTag(variant!)
+  }))
+  .withConfig({
+    shouldForwardProp: shouldForwardCSSProps(['variant'])
+  })(composer);
+
+export default Typography;
