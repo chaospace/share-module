ce3964dba2393e23ce70d2d744edf0395f58675d
chaospace <ichaospace@gmail.com>
2024-06-20 11:29:34 +0900

    [composer] grid 스타일 오타수정

--- front/src/pages/grid/index.tsx
+++ front/src/pages/grid/index.tsx
@@ -1,20 +1,29 @@
-import { HBox, VBox } from '@/components/elements/Box';
+import { VBox } from '@/components/elements/Box';
+import { Container } from '@/components/elements/Container';
 import Grid from '@/components/elements/Grid';
+import { H } from '@/components/elements/Typography';
 import React from 'react';
 
 function GridApp() {
   return (
     <React.Fragment>
-      <Grid gap={40} height='100vh' placeContent='center'>
-        <HBox>
-          <span>아이템</span>
-          <span>아이템2</span>
-          <span>아이템3</span>
-        </HBox>
-        <VBox>
-          <span>아이템</span>
-          <span>아이템2</span>
-          <span>아이템3</span>
+      <Grid
+        width={600}
+        height={600}
+        gridTemplateAreas={`
+            'header header header'
+            'sidebar content content'
+            'sidebar content content'
+          `}>
+        <Container gridArea='header' bgColor='#eee' p={4}>
+          헤더
+          <H variant='subTitle1'>그리드 area를 이용한 레이아웃</H>
+        </Container>
+        <VBox gridArea='sidebar' bgColor='#bbb'>
+          사이드바
+        </VBox>
+        <VBox gridArea='content' bgColor='#aaa'>
+          컨텐츠
         </VBox>
       </Grid>
     </React.Fragment>
